0fe503b16390a9c9ca408d69a0e1f14916c664d1
aidan <aihunt@bas.ac.uk>
2022-08-19 14:33:36 +0100

    Atkinson 2012 data on krill faecal pellet dimensions

--- MatLab/risk_map.m
+++ MatLab/risk_map.m
@@ -22,11 +22,11 @@ season = 2019;
 months = [10:12, 1:3];
 % Load the seawater density values.
 % Model extraction & density calculations must be done already.
-waterDensity = loadWaterDensity(path_physicalModel, season, months);
+dat = loadWaterDensity(path_physicalModel, season, months);
 
 % Number of lat-lon grid cells
-nlon = length(waterDensity.lon);
-nlat = length(waterDensity.lat);
+nlon = length(dat.lon);
+nlat = length(dat.lat);
 
 %% Set up depth layers
 % The GLORYS model output contains 50 depth layers going to nearly 6000 m,
@@ -55,47 +55,85 @@ wm = 0.5 * (wi(1:end-1) + wi(2:end));          % depth layer midpoints
 
 % The GLORYS model output corresponds to depth layer midpoints. Interpolate
 % the model to match our selected depth layers.
-waterDensity.density = permute(waterDensity.density, [3 1 2 4]); % shift depth to first dimension
-% CHECK THE INTERP OPTIONS TO MAKE SURE THE BOUNDARIES ARE HANDLED WELL...
-waterDensity.density = interp1(waterDensity.depth, waterDensity.density, wm);
-waterDensity.density = permute(waterDensity.density, [2 3 1 4]); % restore dimension order
-waterDensity.depth = wm;
+dat.density = permute(dat.density, [3 1 2 4]); % shift depth to first dimension
+dat.density = interp1(dat.depth, dat.density, wm);
+dat.density = permute(dat.density, [2 3 1 4]); % restore dimension order
+dat.depth = wm;
 
 % Index the ocean grid cells -- for efficiency, other cells may be omitted
 % from calculations.
-aboveSeafloor = ~isnan(waterDensity.density(:,:,:,1)); % index all relevant lon-lat-depth cells
+aboveSeafloor = ~isnan(dat.density(:,:,:,1)); % index all relevant lon-lat-depth cells
 isOceanCell = any(aboveSeafloor, 3); % index all relevant lon-lat cells
 
 %% Calculate sinking speeds
 % The sinking speed equations are written in cgs units.
-shape = 'ellipsoid';  % faecal pellet shape
-% rho_p = 1060 * 1e-3; % faecal pellet density, g / cm^3. [1 kg / m^3 = 10^-3 g / cm^3]
-rho_p = 1300 * 1e-3; % faecal pellet density, g / cm^3. [1 kg / m^3 = 10^-3 g / cm^3]
-mu = 0.00189 * 1e1;  % seawater viscosity, g / cm / s. [1 N s / m^2 = 1 Pa s = 10 g / cm / s]
-g = 9.81 * 1e2;      % acceleration due to gravity, cm / s^2. [1 m / s^2 = 10^2 cm / s^2]
+% Specify parameter values
+pars.mu = 0.00189 * 1e1;  % seawater viscosity, g / cm / s. [1 N s / m^2 = 1 Pa s = 10 g / cm / s]
+pars.g = 9.81 * 1e2;      % acceleration due to gravity, cm / s^2. [1 m / s^2 = 10^2 cm / s^2]
+% See Atkinson et al., 2012, for krill faecal pellet properties
+pars.rho_p = 1116 * 1e-3; % faecal pellet density, g / cm^3. [1 kg / m^3 = 10^-3 g / cm^3]
+% pars.rho_p = 1220 * 1e-3; % faecal pellet density, g / cm^3. [1 kg / m^3 = 10^-3 g / cm^3] (value from Komar 1980)
+
+% Faecal pellet dimensions (cm) crudely approximated from Komar et al 1981.
+% Range of euphausiid faecal pellet volume measured by Komar is approx
+% 1e-5 -> 5e-5. Antarctic krill likely near the upper side of this range.
+% This is corroborated by Schmidt et al 2012, who use a standard pellet
+% volume of 0.05 mm^3 = 5e-5 cm^3.
+% pars.V = 5e-5;
+
+% Atkinson et al 2012 provide more detail on krill faecal pellet
+% dimensions, giving a larger median volume.
+% pars.V = 1e-3 * 0.0754; % cm^3
+
+pars.shape = 'cylinder';  % faecal pellet shape (may be cylinder or ellipsoid)
+% It seems that the Atkinson data refer to cylindrical pellets
+pars.L = 2667 * 1e-4; % median length (cm)
+pars.D = 178 * 1e-4; % median width (cm)
 
 % Faecal pellet dimensions, cm.
 % For now, let's assume that faecal pellets are either cylindrical OR
 % ellipsoidal. We may introduce a mixture of shapes later...
-% The values have been crudely approximated from Komar et al. 1981.
-switch shape
+switch pars.shape
     case 'cylinder'
-        L = 0.09847; % length
-        D = 0.01969; % diameter
-        Ds = []; Di = []; Dl = [];
-        V = 0.25 * pi * D ^ 2 * L; % volume
+        pars.Ds = []; pars.Di = []; pars.Dl = [];
+        pars.V = 0.25 * pi * pars.D ^ 2 * pars.L;
+%         x = 15; % assume that length is x times diameter (L = xD)
+%         pars.D = ((4 * pars.V) / (pi * x)) ^ (1 / 3); % diameter
+%         pars.L = x * pars.D; % length
+%         pars.Ds = []; pars.Di = []; pars.Dl = [];
     case 'ellipsoid'
-        Dl = 0.09847; % largest principal axis (diameter)
-        Di = 0.03411; % intermediate
-        Ds = 0.01706; % smallest
-        L = []; D = [];
-        V = pi/6 * Dl * Di * Ds; % volume
+        pars.Ds = pars.D; pars.Di = pars.D;
+        pars.Dl = pars.L;
+        pars.V = 1 / 6 * pi * pars.Ds * pars.Di * pars.Dl;
+%         % assume that the two smallest diameters are equal (Di = Ds), and
+%         % that the largest diameter is x times the smallest (Dl = xDi = xDs)
+%         x = 5;
+%         pars.Ds = ((6 * pars.V) / (pi * x)) ^ (1 / 3); % smallest principal axis (diameter)
+%         pars.Di = pars.Ds; % intermediate
+%         pars.Dl = x * pars.Ds; % largest
+%         pars.L = []; pars.D = [];
 end
 
 % Find faecal pellet sinking speed, cm / s
-[v, Re] = sinking_speed(shape, 1e-3 * waterDensity.density, rho_p, mu, g, ...
-    'L', L, 'D', D, 'Dl', Dl, 'Di', Di, 'Ds', Ds, ...
+[v, Re] = sinking_speed(pars.shape, 1e-3 * dat.density, pars.rho_p, pars.mu, pars.g, ...
+    'L', pars.L, 'D', pars.D, 'Dl', pars.Dl, 'Di', pars.Di, 'Ds', pars.Ds, ...
     'useMeans_Re', true, 'returnMax_Re', true);
+
+
+
+%% Include plastics
+
+% Plastic dimensions. These may be comparable to the faecal pellet
+% dimensions. Look up a paper that shows plastic particles inside faecal
+% pellet and get approx dimensions from there.
+% The plastics that are excreted may be smaller than those that are eaten
+% because they are ground down to smaller sizes. An ingested piece of
+% plastic may not be excreted all at once, it be excreted gradually as
+% smaller pieces.
+
+Vp = 229972525; % mu m^3
+1e-12 * Vp
+
 
 
 
