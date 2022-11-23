28c2ae6383a42dd87d83e5d04a7076b7c7442417
aidan <aihunt@bas.ac.uk>
2022-11-23 10:29:26 +0000

    Include chlorophyll layer -- choice of resolution

--- R/map/shiny/app.R
+++ R/map/shiny/app.R
@@ -20,6 +20,8 @@ nc$surface = factor(nc$surface, levels = unique(nc$surface))
 crs_world = 4326
 crs_use = 3031
 
+
+
 # Load plastic data -----------------------------------------------------
 filename = 'plastic_quantity.csv'
 filepath = 'data/plastic_quantity/'
@@ -63,6 +65,7 @@ DATA$y = xy[2,]
 #                   max(bbox_map$ymax, bbox_dat$ymax)), names(bbox_map))
 # aspectRatio = diff(bbox[c(1,3)]) / diff(bbox[c(2,4)])
 
+
 # Load research station data ----------------------------------------------
 filename = 'COMNAP_Antarctic_Facilities_Master.csv'
 filepath = 'data/research stations/COMNAP/'
@@ -81,6 +84,7 @@ STATIONS$y = xy[2,]
 
 
 
+
 # Load krill data ---------------------------------------------------------
 
 filename = 'krill_data_mapped.csv'
@@ -94,6 +98,9 @@ KRILL_allMonths <- aggregate(value ~ lonmin + lonmax + latmin + latmax, KRILL, '
 KRILL_allMonths$month <- 'all'
 KRILL_allMonths <- KRILL_allMonths[,c(1:4,6,5)]
 KRILL <- rbind(KRILL, KRILL_allMonths)
+# Round to 2 significant figures
+sigDigits <- 2
+KRILL$value <- signif(KRILL$value, sigDigits)
 
 # I think this function is too complicated -- maybe only need call to Polygon function, and not the Polygons function...
 createPolygons <- function(dat){
@@ -135,6 +142,39 @@ for(i in 1:ncols){
 krill_poly$colourgroup <- factor(krill_poly$colourgroup, levels = colgroup)
 
 
+
+# Load chlorophyll data ---------------------------------------------------------
+
+# The high-res data are too big for fast processing... It's quite slow to load
+# and later functions are VERY slow -- I don't know where the bottle neck is.
+# Perhaps dplyr could help... but it's probably best to simply reduce the
+# resolution.
+
+# filename = 'chl_data_mapped.csv'
+filename = 'chl_data_mapped_highRes_3x1.csv'
+# filename = 'chl_data_mapped_highRes_1x0.33.csv'
+filepath = 'MatLab/temp/'
+CHL = read.csv(paste0(filepath, filename), stringsAsFactors = TRUE)
+
+# Data pre-processing -----------------------------------------------------
+CHL <- CHL[!is.nan(CHL$value),] # omit missing data
+# Mean abundance across months for each grid cell
+CHL_allMonths <- aggregate(value ~ lonmin + lonmax + latmin + latmax, CHL, 'mean')
+CHL_allMonths$month <- 'all'
+CHL_allMonths <- CHL_allMonths[,c(1:4,6,5)]
+CHL <- rbind(CHL, CHL_allMonths)
+
+# Round to 2 significant figures
+CHL$value <- signif(CHL$value, sigDigits)
+
+chl_poly <- createPolygons(CHL) # generate polygons
+chl_poly <- SpatialPolygons(chl_poly, proj4string = CRS(paste0('+init=epsg:', as.character(crs_world)))) # create spatial object
+chl_poly <- st_as_sf(chl_poly)
+chl_poly <- st_transform(chl_poly, crs = crs_use) # convert coordinates
+chl_poly$month <- CHL$month
+chl_poly$value <- CHL$value
+
+
 # Plotting parameters -----------------------------------------------------
 # Plot symbols - store in separate data frame
 # Research stations use line-based symbols (crosses/hatches)
@@ -167,6 +207,7 @@ bbox <- setNames(c(min(bbox_map$xmin, bbox_dat$xmin, bbox_krill$xmin),
                    max(bbox_map$xmax, bbox_dat$xmax, bbox_krill$xmax),
                    max(bbox_map$ymax, bbox_dat$ymax, bbox_krill$ymax)), names(bbox_map))
 aspectRatio = diff(bbox[c(1,3)]) / diff(bbox[c(2,4)])
+
 
 
 
@@ -225,10 +266,15 @@ ui <- fluidPage(
       # Input: background layers ----
       radioButtons("background", "Background data:",
                    c("None" = "none",
-                     "Krill (all months)" = "all",
-                     "Krill (Jan)" = "1",
-                     "Krill (Feb)" = "2",
-                     "Krill (Mar)" = "3"),
+                     "Krill (Jan--Mar)" = "krill_all",
+                     "Krill (Jan)" = "krill_1",
+                     "Krill (Feb)" = "krill_2",
+                     "Krill (Mar)" = "krill_3",
+                     "Chlorophyll (Jan--Mar)" = "chl_all",
+                     "Chlorophyll (Jan)" = "chl_1",
+                     "Chlorophyll (Feb)" = "chl_2",
+                     "Chlorophyll (Mar)" = "chl_3"
+                     ),
                    selected = 'none'),
 
       
@@ -309,20 +355,21 @@ server <- function(input, output, session) {
       )
     )
   })
-
-  # Filter the krill data
-  filtered_krill_data = reactive({
-    return(
-      subset(krill_poly, month == input$background)
-    )
-  })
   
-  background_dat <-reactive({
-    if(input$background %in% c('all','1','2','3')){
-      return('krill')
-    }else{
-      return('none')
+  # Filter background data
+  filtered_background_dat <- reactive({
+    backgroundOptions <- c('none', 'krill', 'chl')
+    t <- sapply(backgroundOptions, FUN = function(z) grepl(z, input$background))
+    background <- names(which(t)) # get the chosen background
+    background_dat <- switch(background,
+                             krill = krill_poly,
+                             chl = chl_poly
+    ) # get background data
+    if(background != 'none'){
+      m <- strsplit(input$background, '_')[[1]][2] # get the chosen month
+      background_dat <- subset(background_dat, month == m) # filter by month
     }
+    return(list(background, background_dat))
   })
   
   # Filter plot symbols based on sample & station types
@@ -356,9 +403,20 @@ server <- function(input, output, session) {
     
     dat_plastic <- filtered_plastic_data()
     dat_stations <- filtered_station_data()
-    dat_krill <- filtered_krill_data()
-    background <- background_dat()
+    background_info <- filtered_background_dat()
+    background <- background_info[[1]]
+    dat_background <- background_info[[2]]
     symbols <- Symbols()
+    
+    # dat_plastic <- DATA_sf
+    # dat_stations <- STATIONS_sf
+    # dat_krill <- krill_poly
+    # dat_chl <- chl_poly
+    # background <- 'chl'
+    # symbols <- pltSymbols
+    
+    anyPlastic <- nrow(dat_plastic) > 0
+    anyStations <- nrow(dat_stations) > 0
     
     # Background layer
     anyBackground <- input$background != 'none'
@@ -366,12 +424,20 @@ server <- function(input, output, session) {
            krill = {
              plt_background <- 
                ggplot() +
-               geom_sf(data = dat_krill, aes(fill = colourgroup)) +
-               scale_fill_viridis_d(option = 'plasma') +
-               guides(fill = guide_legend(title = bquote(atop(Krill, individuals / m^2))))
+               geom_sf(data = dat_background, aes(fill = colourgroup)) +
+               scale_fill_viridis_d(option = 'plasma',
+                                    name = bquote(atop(Krill, individuals / m^2)))
+               # guides(fill = guide_legend(title = bquote(atop(Krill, individuals / m^2))))
+           },
+           chl = {
+             plt_background <- 
+               ggplot() +
+               geom_sf(data = dat_background, aes(fill = value)) +
+               scale_fill_viridis_c(option = 'viridis', trans = 'log10',
+                                    name = bquote(atop(Chlorophyll, mg / m^3)))
            }
     )
-
+    
     # Coastline
     if(background == 'none'){
       plt_map <- 
@@ -392,46 +458,53 @@ server <- function(input, output, session) {
 
     
     # Research stations
-    plt_stations <- 
-      ggplot() +
-      geom_sf(data = dat_stations,
-              aes(shape = Type, colour = Seasonality),
-              alpha = 1, size = 4, stroke = 1) +
-      scale_colour_manual(values = c('forestgreen','firebrick')) +
-      scale_shape_manual(values = symbols$symbol[symbols$Class == 'ResearchStation']) +
-      # scale_shape_manual(values = Symbols()$symbol[Symbols()$Class == 'ResearchStation']) +
-      # scale_shape_manual(values = c(3,4,8,7,9,10)) +
-      # guides(shape = guide_legend(title = 'Facility', override.aes = list(shape = unique(dat_stations$symbol))),
-      #        colour = guide_legend(override.aes = list(colour = c('forestgreen','firebrick'))))
-      guides(shape = guide_legend(title = 'Facility'), 
-             colour = guide_legend(override.aes = list(colour = c('forestgreen','firebrick'), shape = 3)))
+    if(anyStations){
+      plt_stations <- 
+        ggplot() +
+        geom_sf(data = dat_stations,
+                aes(shape = Type, colour = Seasonality),
+                alpha = 1, size = 4, stroke = 1) +
+        scale_colour_manual(values = c('forestgreen','firebrick')) +
+        scale_shape_manual(values = symbols$symbol[symbols$Class == 'ResearchStation']) +
+        # scale_shape_manual(values = Symbols()$symbol[Symbols()$Class == 'ResearchStation']) +
+        # scale_shape_manual(values = c(3,4,8,7,9,10)) +
+        # guides(shape = guide_legend(title = 'Facility', override.aes = list(shape = unique(dat_stations$symbol))),
+        #        colour = guide_legend(override.aes = list(colour = c('forestgreen','firebrick'))))
+        guides(shape = guide_legend(title = 'Facility'), 
+               colour = guide_legend(override.aes = list(colour = c('forestgreen','firebrick'), shape = 3)))
+    }
     
     # Plastic samples
-    plt_plastic_samples <- 
-      ggplot() +
-      geom_sf(data = dat_plastic,
-              aes(fill = Source, shape = SampleType),
-              alpha = 1, size = 4) +
-      scale_fill_manual(values = setNames(pltColours$colour, pltColours$Source)) +
-#      scale_colour_brewer(type = 'qual', palette = 'Set1', aesthetics = c('fill')) +
-      scale_shape_manual(values = symbols$symbol[symbols$Class == 'PlasticSample']) +
-      guides(shape = guide_legend(title = 'Sample type'),
-             fill = guide_legend(override.aes = list(shape = c(21))))
+    if(anyPlastic){
+      plt_plastic_samples <- 
+        ggplot() +
+        geom_sf(data = dat_plastic,
+                aes(fill = Source, shape = SampleType),
+                alpha = 1, size = 4) +
+        scale_fill_manual(values = setNames(pltColours$colour, pltColours$Source)) +
+        #      scale_colour_brewer(type = 'qual', palette = 'Set1', aesthetics = c('fill')) +
+        scale_shape_manual(values = symbols$symbol[symbols$Class == 'PlasticSample']) +
+        guides(shape = guide_legend(title = 'Sample type'),
+               fill = guide_legend(override.aes = list(shape = c(21))))
       # guides(fill = guide_legend(override.aes = list(shape = c(21))))
-    
+    }
     
     # Extract legends
-    leg_stations <- gtable_filter(ggplot_gtable(ggplot_build(plt_stations)), "guide-box") 
-    leg_stations_grob <- grobTree(leg_stations)
+    if(anyStations){
+      leg_stations <- gtable_filter(ggplot_gtable(ggplot_build(plt_stations)), "guide-box") 
+      leg_stations_grob <- grobTree(leg_stations)
+    }
     
-    leg_plastic_samples <- gtable_filter(ggplot_gtable(ggplot_build(plt_plastic_samples)), "guide-box") 
-    leg_plastic_samples_grob <- grobTree(leg_plastic_samples)
+    if(anyPlastic){
+      leg_plastic_samples <- gtable_filter(ggplot_gtable(ggplot_build(plt_plastic_samples)), "guide-box") 
+      leg_plastic_samples_grob <- grobTree(leg_plastic_samples)
+    }
     
     # Combine plot layers
     plt_no_legend <- 
       
       # Coastline & (optional) background
-      plt_map +
+      plt_map
       
       # ggplot() + 
       
@@ -441,27 +514,37 @@ server <- function(input, output, session) {
       #         show.legend = FALSE) +
       # scale_fill_manual(values = c('grey','skyblue','skyblue','grey')) +
       
-      scale_shape_manual(values = setNames(symbols$symbol, symbols$Type)) +
+    if(anyStations | anyPlastic){
+      plt_no_legend <- plt_no_legend +
+        scale_shape_manual(values = setNames(symbols$symbol, symbols$Type))
+    }
       
-      # Research stations
+    # Research stations
+    if(anyStations){
+      plt_no_legend <- plt_no_legend +
       geom_sf(data = dat_stations,
               aes(shape = Type, colour = Seasonality),
               alpha = 1, size = 4, stroke = 1, show.legend = FALSE) +
-      scale_colour_manual(values = c('forestgreen','firebrick')) +
-      # scale_shape_manual(values = symbols$symbol) +
-      # scale_shape_manual(values = symbols$symbol[symbols$Class == 'ResearchStation']) +
-      # scale_shape_manual(values = Symbols()$symbol) + # IT'S CRUCIAL TO ENSURE THAT THE FACTOR ORDER IS CONSISTENT BETWEEN DATA SETS AND THE SYMBOLS DATA SET
-#      scale_shape_manual(values = c(3,4,8,7,9,10,21:25)) + # 1st 6 values for stations, remaining values for plastic samples
+        scale_colour_manual(values = c('forestgreen','firebrick'))
+        # scale_shape_manual(values = symbols$symbol) +
+        # scale_shape_manual(values = symbols$symbol[symbols$Class == 'ResearchStation']) +
+        # scale_shape_manual(values = Symbols()$symbol) + # IT'S CRUCIAL TO ENSURE THAT THE FACTOR ORDER IS CONSISTENT BETWEEN DATA SETS AND THE SYMBOLS DATA SET
+        #      scale_shape_manual(values = c(3,4,8,7,9,10,21:25)) + # 1st 6 values for stations, remaining values for plastic samples
+    }
       
-      # Plastic samples
+    # Plastic samples
+    if(anyPlastic){
+      plt_no_legend <- plt_no_legend +
       new_scale('fill') +
-      geom_sf(data = dat_plastic,
-              aes(fill = Source, shape = SampleType),
-              alpha = 1, size = 4, show.legend = FALSE) +
-      scale_fill_manual(values = setNames(pltColours$colour, pltColours$Source)) +
+        geom_sf(data = dat_plastic,
+                aes(fill = Source, shape = SampleType),
+                alpha = 1, size = 4, show.legend = FALSE) +
+        scale_fill_manual(values = setNames(pltColours$colour, pltColours$Source))
       # scale_colour_brewer(type = 'qual', palette = 'Set1', aesthetics = c('fill')) +
       # scale_shape_manual(values = symbols$symbol[symbols$Class == 'PlasticSample']) +
+    }
       
+    plt_no_legend <- plt_no_legend +
       coord_sf(xlim = c(bbox['xmin'], bbox['xmax']), ylim = c(bbox['ymin'], bbox['ymax'])) +
       theme_void()
     
@@ -476,18 +559,28 @@ server <- function(input, output, session) {
         axis.ticks = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank() 
-      ) +
-      annotation_custom(leg_plastic_samples_grob, xmin = 0.5, xmax = 0.5, ymin = 0.5, ymax = 1) + 
-      annotation_custom(leg_stations_grob, xmin = 0.35, xmax = 0.35, ymin = 0, ymax = 0.5) 
-    
-    
+      )
+    if(anyPlastic){
+      legs <- legs +
+        annotation_custom(leg_plastic_samples_grob, xmin = 0.5, xmax = 0.5, ymin = 0.5, ymax = 1)
+    }
+    if(anyStations){
+      legs <- legs +
+        annotation_custom(leg_stations_grob, xmin = 0.35, xmax = 0.35, ymin = 0, ymax = 0.5) 
+    }
+
     # Use cowplot package to arrange plot
     # plt <- ggdraw(plt_no_legend, clip = 'on')# + 
     plt <- ggdraw() + 
-      draw_plot(plt_no_legend, 0, 0, 0.7, 1) +
-      draw_plot(leg_plastic_samples_grob, 0.7, 0, 0.15, 1) +
-      draw_plot(leg_stations_grob, 0.85, 0, 0.15, 1)
-    #      draw_plot(legs, 0.8, 0, 0.2, 1)
+      draw_plot(plt_no_legend, 0, 0, 0.7, 1)
+    if(anyPlastic){
+      plt <- plt +
+        draw_plot(leg_plastic_samples_grob, 0.7, 0, 0.15, 1)
+    }
+    if(anyStations){
+      plt <- plt +
+        draw_plot(leg_stations_grob, 0.85, 0, 0.15, 1)
+    }
    plt
     
     
